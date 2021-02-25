4226932f978c5f8ffa17542c1055c7a4d141033d
Cory <Kelly>
2021-02-24 18:57:50 -0600

    CORS handling

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ const corsOptions = {
 }
 app.options('*', cors())
 cors(corsOptions)
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+})
 app.use(morgan('dev'))
 app.use(expressCspHeader({
   directives: {
