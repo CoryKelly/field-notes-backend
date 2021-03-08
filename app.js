40c3448815cd15eddf97258e0bbf4da4ab09b92e
Cory <Kelly>
2021-03-08 09:14:17 -0600

    - Initial Commit

--- app.js
+++ app.js
@@ -12,19 +12,29 @@ const port = process.env.PORT || 3000
 const postRoutes = require('./api/routes/post')
 
 // Middleware
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-})
 app.use(morgan('dev'))
 app.use(expressCspHeader({
   directives: {
-    'img-src': ['static', 'localhost:3000']
+    'img-src': ['static', 'https://filed-notes-app-backend.herokuapp.com']
   }
 }));
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  //intercepts OPTIONS method
+  if ('OPTIONS' === req.method) {
+    //respond with 200
+    res.send(200);
+  }
+  else {
+    //move on
+    next();
+  }
+});
+
 app.use('/static/', express.static('static'))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
