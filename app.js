489f7295926dba643ac29d6e116ddce1c817e163
Cory <Kelly>
2021-03-08 10:01:17 -0600

    - Initial Commit

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  console.log('CORS BABY!')
 
   //intercepts OPTIONS method
   if ('OPTIONS' === req.method) {
